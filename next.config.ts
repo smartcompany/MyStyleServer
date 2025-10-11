022df8e51bec34cd6acf81b0a6cd44b8f6d15b4c
yong geon <gunnylove@gmail.com>
2025-10-11 14:29:30 +0900

    update

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  experimental: {
-    serverComponentsExternalPackages: ['axios'],
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
+  /* config options here */
 };
 
 export default nextConfig;
